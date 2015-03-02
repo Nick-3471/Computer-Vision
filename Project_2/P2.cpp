9d53bfc22e0e4984957dc4c807b37993ef25890b
Nick <nick_3471@yahoo.com>
2015-03-01 21:39:28 -0800

    So many questions

--- Project_2/P2.cpp
+++ Project_2/P2.cpp
@@ -13,9 +13,56 @@ using namespace std;
 int main()
 {
 
+int** myImage;
+char in[20] = "1.pgm";
+char out[20] = "image.pgm";
+
+int** ImageOut;
+
+
+int M, N, Q;
+
+int LEX, LEY;
+int REX, REY;
+int NX, NY;
+int MX, MY;
 
 
 
+cout << endl << "Enter Left Eye X: ";
+cin >> LEX;
+cout << "Enter Left Eye y: ";
+cin >> LEY;
+cout  << "Enter Right Eye X: ";
+cin >> REX;
+cout  << "Enter Right Eye y: ";
+cin >> REY;
+cout << "Enter Nose X: ";
+cin >> NX;
+cout  << "Enter Nose y: ";
+cin >> NY;
+cout << "Enter Mouth x: ";
+cin >> MX;
+cout  << "Enter Mouth y: ";
+cin >> MY;
+
+ReadImage(in, &myImage, M, N, Q);
+
+ImageOut = new int* [40];
+for(int i = 0; i < 40; i++)
+{
+	(ImageOut)[i]= new int[48];
+}
+
+for(int q = 0; q < 40; q++)
+{
+	for(int z = 0; z < 48; z++)
+	{
+		ImageOut[q][z] = myImage[LEX + q - 6][LEY + z];
+	}
+}
+ WriteImage(out, ImageOut, M, N ,Q);
+cout << "Got here" << endl;
 
 	return 0;
 }
